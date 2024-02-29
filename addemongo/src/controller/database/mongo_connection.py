9060056ad7f8d26a5dd5bcfbcf628b10cda0689e
FernandoNogueiraPy <fernandomoltt101@gmail.com>
2024-02-29 11:41:58 -0300

    feat:  Create MongoConnection Class for methods, connec/database/collection

---
+++ addemongo/src/controller/database/mongo_connection.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from typing import Any,Sequence
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+class MongoConnection():
+    
+    def __init__(self, host: str | Sequence[str] | None = None, port: int | None = None,**kwargs: Any) -> None:
+
+        self.__client = AsyncIOMotorClient(host, port,**kwargs)
+
+    def set_database(self,name:str) -> MongoConnection:
+        self.database =  self.__client.get_database(name=name)
+        return self
+
+    def set_collection(self,name:str) -> MongoConnection:
+
+        if not hasattr(self,"database"):
+            raise AttributeError("Database not set")
+        
+        self.collection = self.database.get_collection(name=name)
+        return self
+    
+
