7aeec2a2f3a7c96ddfb9151b5346c65edad67a12
FernandoNogueiraPy <fernandomoltt101@gmail.com>
2024-02-29 11:49:21 -0300

    Refactor MongoConnection class***
    This commit refactors the `MongoConnection` class in `mongo_connection.py`. The changes include:
    style: - Formatting improvements for better readability
    - Adding type hints for parameters and return types
    - Renaming variables for clarity
    - Adding exception handling for missing database

--- addemongo/src/controller/database/mongo_connection.py
+++ addemongo/src/controller/database/mongo_connection.py
@@ -1,24 +1,29 @@
 from __future__ import annotations
-from typing import Any,Sequence
+
+from typing import Any, Sequence
+
 from motor.motor_asyncio import AsyncIOMotorClient
 
 
-class MongoConnection():
-    
-    def __init__(self, host: str | Sequence[str] | None = None, port: int | None = None,**kwargs: Any) -> None:
+class MongoConnection:
 
-        self.__client = AsyncIOMotorClient(host, port,**kwargs)
+    def __init__(
+        self,
+        host: str | Sequence[str] | None = None,
+        port: int | None = None,
+        **kwargs: Any,
+    ) -> None:
 
-    def set_database(self,name:str) -> MongoConnection:
-        self.database =  self.__client.get_database(name=name)
+        self.__client = AsyncIOMotorClient(host, port, **kwargs)
+
+    def set_database(self, name: str) -> MongoConnection:
+        self.database = self.__client.get_database(name=name)
         return self
 
-    def set_collection(self,name:str) -> MongoConnection:
+    def set_collection(self, name: str) -> MongoConnection:
 
-        if not hasattr(self,"database"):
+        if not hasattr(self, "database"):
             raise AttributeError("Database not set")
-        
+
         self.collection = self.database.get_collection(name=name)
         return self
-    
-
