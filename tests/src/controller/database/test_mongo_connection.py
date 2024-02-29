f3409d9fe502eb7042858d800a0c8e90978010f1
FernandoNogueiraPy <fernandomoltt101@gmail.com>
2024-02-29 11:43:35 -0300

    test: Add test for MongoConnection class

---
+++ tests/src/controller/database/test_mongo_connection.py
@@ -0,0 +1,16 @@
+from pytest_mock import MockFixture
+from typing import Any
+from addemongo.src.controller.database.mongo_connection import MongoConnection
+from motor.motor_asyncio import AsyncIOMotorClient
+
+class MongoConnectionSuite:
+
+    def test_should_intance_async_motorclient_in_init(self,mocker: MockFixture):
+
+        def stub_init(*arg:Any,**kwargs: Any) -> None:
+            pass
+
+        mocker.patch.object(AsyncIOMotorClient,"__init__",stub_init)
+        init_spy = mocker.spy(AsyncIOMotorClient,"__init__")
+        MongoConnection()
+        init_spy.assert_called_once()
